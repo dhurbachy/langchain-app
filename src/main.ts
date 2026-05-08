31f53a6bd99af8b8efc44dbb30a92f94b50b8dd7
dhrub chy <dhurbac66@gmail.com>
2026-05-08 21:39:34 +0545

    configure and Setup swagger and fastify

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter() as any);
+
+  const config=new DocumentBuilder()
+  .setTitle('AI Powered Langchain App')
+  .setDescription('Complete AI Powered Langchain Application')
+  .setVersion('v1')
+  .build();
+  const document=SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();

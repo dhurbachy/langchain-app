cff8a0c78e063dc3f9e55da51364deb48096d32d
dhrub chy <dhurbac66@gmail.com>
2026-05-08 22:40:11 +0545

    feat: integrate ollama

--- src/common-ai/common-ai.module.ts
+++ src/common-ai/common-ai.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CommonAiService } from './common-ai.service';
 
 @Module({
-  providers: [CommonAiService]
+  providers: [CommonAiService],
+  exports:[CommonAiService]
 })
 export class CommonAiModule {}
--- src/common-ai/common-ai.service.ts
+++ src/common-ai/common-ai.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Ollama } from "@langchain/ollama"
 
 @Injectable()
-export class CommonAiService {}
+export class CommonAiService { 
+    private model:Ollama;
+
+    constructor(){
+        this.model=new Ollama({
+            model:'gemma:2b',
+            temperature:0.5,
+            baseUrl:'http://localhost:11434'
+        })
+    }
+
+    async chat(prompt:string){
+        try{
+            return await this.model.invoke(prompt);
+
+        }catch(error:any){
+          return `Error Connecting to Ollama ${error.message}`;
+        }
+    }
+}
